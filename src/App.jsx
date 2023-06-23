3ed37bad7bb4a0920781f98d06f956dd6f0c3863
Edson Santos <dev@escode.net>
2023-06-23 13:04:52 -0300

    Iniciando com useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import React from "react";
 
 function App() {
-  return <div></div>;
+  const [ativo, setAtivo] = React.useState(true);
+
+  return (
+    <button onClick={() => setAtivo(!ativo)}>
+      {ativo ? "Ativo" : "Inativo"}
+    </button>
+  );
 }
 
 export default App;
